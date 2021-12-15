536c8e899b70d852abdf4a9bd12506c0dac62117
Michael Brandt <msbrandt00@gmail.com>
2021-12-15 09:49:00 -0700

    Features: add #drive? and #driver? methods

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,12 +1,21 @@
 class Passenger
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :driver
   def initialize(hash)
     @name = hash['name']
     @age = hash['age']
+    @driver = false
   end
 
   def adult?
     @age >=18
+  end
+
+  def drive
+    @driver = true
+  end
+
+  def driver?
+    @driver
   end 
 
 end
