40bc06e87ca9153190ffea43dcac9451ad087fa0
Michael Brandt <msbrandt00@gmail.com>
2021-12-15 09:44:45 -0700

    Test: Passenger class will read age and name

---
+++ spec/passenger_spec.rb
@@ -0,0 +1,18 @@
+require './lib/passenger'
+
+RSpec.describe do
+  before(:each) do
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+  end
+
+  it 'will recognize names' do
+    expect(@charlie.name).to eq('Charlie')
+    expect(@taylor.name).to eq('Taylor')
+  end
+
+  it 'will recognize ages' do
+    expect(@charlie.age).to eq(18)
+    expect(@taylor.age).to eq(12)
+  end 
+end
