b8f0779290774ebaa6dfc710dc4f2f7dfe9fdf8c
Michael Brandt <msbrandt00@gmail.com>
2021-12-15 10:02:33 -0700

    Add #add_passenger method

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,11 +1,13 @@
 class Vehicle
   attr_reader :year, :make, :model
+  attr_accessor :passengers
 
   def initialize(year, make, model)
     @year = year
     @make = make
     @model = model
     @speeding = false
+    @passengers = []
   end
 
   def speeding?
@@ -14,5 +16,9 @@ class Vehicle
 
   def speed
     @speeding = true
-  end 
+  end
+
+  def add_passenger(passenger)
+    @passengers << passenger
+  end
 end
